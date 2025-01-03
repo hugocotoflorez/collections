b89fadea1540323ca4997aac58038abdd0b4d2c0
hugocotoflorez <hugocoto100305@gmail.com>
2025-01-03 11:58:08 +0100

    namespace

--- misc.h
+++ misc.h
@@ -2,3 +2,11 @@
 #include <string.h>
 
 #define objcpy(obj) memcpy(malloc(sizeof(obj)), &obj, sizeof(obj));
+
+/* Add a function to OBJ namespace */
+#define NAMESPACE_ADD(obj, func) typeof obj##_func *func = obj##_func
+
+/* Initialize OBJ namespace */
+#define namespace(obj)           \
+        NAMESPACE_ADD(obj, add); \
+        NAMESPACE_ADD(obj, remove);
